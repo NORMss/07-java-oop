389cefcbc859703a863b8244999ce7934be3aee4
NORMss <serega.b301@gmail.com>
2024-09-27 22:03:48 +0700

    feat: the implementation of the Post class has been extended in accordance with the [Post](https://eltex-android.ru/swagger-ui/index.html) class

--- src/main/java/com/eltex/Main.java
+++ src/main/java/com/eltex/Main.java
@@ -5,8 +5,8 @@ import com.eltex.data.WallService;
 public class Main {
     public static void main(String[] args) {
         final var service = new WallService();
-        service.like();
-
+        System.out.println(service.getPost());
+        service.setLikedByMe(true);
         System.out.println(service.getPost());
     }
 }
--- src/main/java/com/eltex/data/Post.java
+++ src/main/java/com/eltex/data/Post.java
@@ -1,21 +1,70 @@
 package com.eltex.data;
 
-public record Post(String content, String author, int likes) {
+public record Post(
+        int id,
+        int authorId,
+        String author,
+        String authorJob,
+        String authorAvatar,
+        String content,
+        String published,
+        String link,
+        boolean mentionedMe,
+        boolean likedByMe
+) {
 
     public Builder builder() {
-        return new Builder()
+        return new Builder(id, authorId, author, content, published, mentionedMe, likedByMe)
+                .setId(id)
+                .setAuthorId(authorId)
                 .setAuthor(author)
+                .setAuthorJob(authorJob)
+                .setAuthorAvatar(authorAvatar)
                 .setContent(content)
-                .setLikes(likes);
+                .setPublished(published)
+                .setLink(link)
+                .setMentionedMe(mentionedMe)
+                .setLikedByMe(likedByMe);
     }
 
     static class Builder {
-        private String content = "";
-        private String author = "";
-        private int likes = 0;
+        private int id;
+        private int authorId;
+        private String author;
+        private String content;
+        private String published;
+        private boolean mentionedMe;
+        private boolean likedByMe;
+        private String authorJob;
+        private String authorAvatar;
+        private String link;
 
-        public Builder setContent(final String content) {
+        public Builder(
+                final int id,
+                final int authorId,
+                final String author,
+                final String content,
+                final String published,
+                final boolean mentionedMe,
+                final boolean likedByMe
+        ) {
+            this.id = id;
+            this.authorId = authorId;
+            this.author = author;
             this.content = content;
+            this.published = published;
+            this.mentionedMe = mentionedMe;
+            this.likedByMe = likedByMe;
+        }
+
+
+        public Builder setId(final int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setAuthorId(final int authorId) {
+            this.authorId = authorId;
             return this;
         }
 
@@ -24,13 +73,52 @@ public record Post(String content, String author, int likes) {
             return this;
         }
 
-        public Builder setLikes(final int likes) {
-            this.likes = likes;
+        public Builder setAuthorJob(final String authorJob) {
+            this.authorJob = authorJob;
+            return this;
+        }
+
+        public Builder setAuthorAvatar(final String authorAvatar) {
+            this.authorAvatar = authorAvatar;
+            return this;
+        }
+
+        public Builder setContent(final String content) {
+            this.content = content;
+            return this;
+        }
+
+        public Builder setPublished(final String published) {
+            this.published = published;
+            return this;
+        }
+
+        public Builder setLink(final String link) {
+            this.link = link;
+            return this;
+        }
+
+        public Builder setMentionedMe(final boolean mentionedMe) {
+            this.mentionedMe = mentionedMe;
+            return this;
+        }
+
+        public Builder setLikedByMe(final boolean likedByMe) {
+            this.likedByMe = likedByMe;
             return this;
         }
 
         public Post build() {
-            return new Post(content, author, likes);
+            return new Post(id,
+                    authorId,
+                    author,
+                    authorJob,
+                    authorAvatar,
+                    content,
+                    published,
+                    link,
+                    mentionedMe,
+                    likedByMe);
         }
     }
 }
